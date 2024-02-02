66b93ba38758cdeea96ecbbc0e3303409b42a2f2
Bruno <delmo123123@gmail.com>
2024-02-02 14:26:26 +0100

    starting git init, ecommerce on django

--- ejangocommerce/manage.py
+++ ejangocommerce/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nordichomes.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ejangocommerce.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
