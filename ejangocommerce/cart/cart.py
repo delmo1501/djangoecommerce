d43367a62f7efc958a4e561c18ae7cd6b9e02f6f
Bruno <delmo123123@gmail.com>
2024-02-08 11:32:16 +0100

    bug with product into cart, product.id into product_id

--- ejangocommerce/cart/cart.py
+++ ejangocommerce/cart/cart.py
@@ -9,35 +9,35 @@ class Cart(object):
 
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
-
+        
         self.cart = cart
-
+    
     def __iter__(self):
         for p in self.cart.keys():
-            self.cart[str(p)]['product'] = Product.objects.get(pk=p) # iterate per product
+            self.cart[str(p)]['product'] = Product.objects.get(pk=p)
     
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values()) # checking quantity in cart
-
+        return sum(item['quantity'] for item in self.cart.values())
+    
     def save(self):
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
     
-    def add(self, product, quantity=1, update_quantity=False):
-        product_id = str(product.id)
+    def add(self, product_id, quantity=1, update_quantity=False):
+        product_id = str(product_id)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 1, 'id': product.id, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 1, 'id': product_id}
         
         if update_quantity:
             self.cart[product_id]['quantity'] += int(quantity)
-            
+
             if self.cart[product_id]['quantity'] == 0:
                 self.remove(product_id)
-        
+            
         self.save()
-
+    
     def remove(self, product_id):
-        if product_id not in self.cart:
+        if product_id in self.cart:
             del self.cart[product_id]
             self.save()
